063d05c6ae61f7f2bc8cc74e6f594b82a75da6b9
DESKTOP-OHD6DA9\USER <zufall2277@gmail.com>
2022-08-09 23:58:26 +0900

    카드 기본적인 세팅 / 표준 카드 만듬

---
+++ Assets/Script/Hearthstone/Card.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CardLevel
+{
+    ÀÏ¹Ý = 0,
+    ÈÖ±Í,
+    ¿µ¿õ,
+    Àü¼³,
+    ¾øÀ½
+};
+
+public class Card : MonoBehaviour
+{
+    public CardData cardData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/Hearthstone/CardData.cs
+++ Assets/Script/Hearthstone/CardData.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [CreateAssetMenu(fileName = "Card Data", menuName = "Scriptable Object/Hearthstone", order = 1)]
 public class CardData : ScriptableObject
 {
-    [Header("Minion, Spell, Weapon, Place")]
+    [Header("0 = Minion, 1 = Spell, 2 = Weapon, 3 = Place")]
     [SerializeField]
     private int cardState;
 
     public int CardState { get { return cardState; } }
 
-    [Header("Warrior, Shaman, Rogue, Paladin, Hunter, Druid, Warlock, Mage, Priest, Demon Hunter")]
+    [Header("Warrior, Shaman, Rogue, Paladin, Hunter, Druid, Warlock, Mage, Priest, Demon Hunter, Nomal")]
     [SerializeField]
     private string cardJob;
 
-    public string CardJob { get { return cardJob; } } 
+    public string CardJob { get { return cardJob; } }
 
+    [Header("0 = 일반, 1 = 휘귀, 2 = 영웅, 3 = 전설, 4 = 없음")]
+    [SerializeField]
+    private int cardLevel;
+
+    public int CardLevel { get { return cardLevel; } }
 
     [SerializeField]
     private string cardName;
@@ -45,6 +51,55 @@ public class CardData : ScriptableObject
     [System.Serializable]
     public struct Keyword
     {
-
+        public int 과부하;
+        public bool 도발;
+        public bool 독성;
+        public bool 돌진;
+        public bool 면역;
+        public bool 발견;
+        public bool 비밀;
+        public bool 빙결;
+        public bool 생명력흡수;
+        public bool 선택;
+        public bool 속공;
+        public bool 연계;
+        public bool 은신;
+        public bool 전투의함성;
+        public int 주문공격력;
+        public bool 죽음의메아리;
+        public bool 질풍;
+        public bool 차단;
+        public bool 천상의보호막;
+        public bool 침묵;
+        public bool 개전;
+        public bool 추방자;
+        public bool 뽑을때시전;
+        public int 휴면;
+        public bool 광폭;
+        public bool 교환성;
+        public bool 퀘스트전개;
+        public bool 명예로운일격;
+        public int 거수;
+        public bool 인양;
+        public int 주입;
+        public bool 예비부품;
+        public bool 격려;
+        public bool 비취골렘;
+        public bool 적응;
+        public bool 퀘스트보상;
+        public bool 소집;
+        public bool 잔상;
+        public bool 합체;
+        public bool 압살;
+        public bool 이중주문;
+        public bool 졸개;
+        public bool 환생;
+        public bool 기원;
+        public bool 부가퀘스트보상;
+        public bool 주문폭주;
+        public bool 타락;
     }
+
+    [Header("Card Keyword")]
+    public Keyword keyword;
 }
